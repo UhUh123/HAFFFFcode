c7052370c16b25336922945e4f4849524cb00fed
NikitaSlvv <NASolovyev1@yandex.ru>
2025-02-20 18:57:58 +0300

    шаг 3

--- haff.h
+++ haff.h
@@ -20,3 +20,22 @@ struct Node {
         : symb(s), freq(f), isSymb(isSymb), level(0), left(nullptr), right(nullptr), next(nullptr) {
     }
 };
+union BIT2CHAR {
+    char symb;
+    struct {
+        unsigned b1 : 1;
+        unsigned b2 : 1;
+        unsigned b3 : 1;
+        unsigned b4 : 1;
+        unsigned b5 : 1;
+        unsigned b6 : 1;
+        unsigned b7 : 1;
+        unsigned b8 : 1;
+    } mbit;
+};
+
+void PodschetChastot(const string& filename, vector<unsigned long>& freq);
+
+void Add2List(Node*& head, Node* newNode);
+
+Node* SortChastot(const vector<unsigned long>& freq);
